369ab871c451c3976b68406c8f6f6388751e46fa
nhat12368 <nhathavps@gmail.com>
2024-06-23 14:36:29 -0700

    library config

--- AndroidConponent/build.gradle.kts
+++ AndroidConponent/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -34,6 +35,17 @@ android {
         enable = true
     }
 }
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
+
 
 dependencies {
 
